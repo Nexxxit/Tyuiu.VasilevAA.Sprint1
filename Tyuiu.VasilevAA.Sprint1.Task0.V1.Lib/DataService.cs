65bbac7b9b1fef1d8dd220e9e8401c358c0f92bd
LostProfile <varsik.arsik@mail.ru>
2023-09-13 21:02:59 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.VasilevAA.Sprint1.Task0.V1.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Tyuiu.VasilevAA.Sprint1.Task0.V1.Lib
+{
+    public class DataService
+    {
+    }
+}
